d3b2a03e6a46a6733df84bdad19b7a1a02157cfd
David <sonhyosuck@gmail.com>
2018-09-08 02:19:24 -0500

    Fix bug to handle board >= 10

--- src/ToyCar.java
+++ src/ToyCar.java
@@ -6,18 +6,37 @@ public class ToyCar {
         int m = board.length;
         int n = board[0].length;
         int reachedBlock = 0;
+        //Tracking variables
+        int digitSum;
+        int rowValue = 0;
         //Go through grid
         for (int i=0; i<m; i++){
-            //Need to set j = 1 because 0,0, 1,0, 2,0.... should not count as reached block
+            //Set total digit sum back to zero
+            digitSum = 0;
+            //Initialize row digit to track integer value >= 10
+            int rowDigits = i;
+            //row digit calculation
+            while (rowDigits>0){
+                digitSum += rowDigits %10;
+                rowValue = digitSum;
+                rowDigits /= 10;
+            }
             for (int j=1; j<n;j++){
-                //Calculate digit sum
-                int digitSum = (i%10)+(i/10)+(j);
+                //Initialize column digit to track integer value >= 10
+                int colDigits = j;
+                //column digit calculation
+                while (colDigits>0){
+                    digitSum += colDigits %10;
+                    colDigits /= 10;
+                }
                 //It will break column when its > x
                 if (digitSum>x){
                     break;
                 }
                 //Increment reached block
                 reachedBlock++;
+                //Keep digit sum to current row value to calculate with column digits
+                digitSum = rowValue;
             }
         }
         return reachedBlock;
